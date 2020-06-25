dd2dc3e594f978e5ccc7cf31ee8265be75287e52
Jason <jason23347@gmail.com>
2020-06-25 12:55:23 +0800

    parser: Remove outdated comments
    Signed-off-by: Jason <jason23347@gmail.com>

--- parser.h
+++ parser.h
@@ -3,7 +3,6 @@
 
 #include "symbols.h"
 
-/* All these functions return 0 for OK and -1 on error */
 void parse();
 void parse_statement(const token_t *token);
 void parse_expresion(const token_t *token);
