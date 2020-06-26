Jason23347/PL0-Analyzer

commit 265071d66d86b62a38e722e1baf68086e66e88a4

parser: Enable muti-definations of procedures
Signed-off-by: Jason <jason23347@gmail.com>

parser.c
@@ -64,6 +64,7 @@ parse()
 		} while (next_token()->type == comma); // ,
 
 		assert(token_tail, semicolon); // ;
+		next_token();
 	}
 
 	if (token_tail->type == varsym) { // var
@@ -72,21 +73,25 @@ parse()
 		} while (next_token()->type == comma); // ,
 
 		assert(token_tail, semicolon); // ;
+		next_token();
 	}
 
-	if (token_tail->type == proceduresym) { // procedure
+	for (; token_tail->type == proceduresym;) { // procedure
 		assert(next_token(), ident); // id
 		assert(next_token(), semicolon); // ;
 
-		parse();
+		parse(); // block
+
+		assert(next_token(), semicolon); // ;
+		next_token();
 	}
 
+	parse_statement(token_tail); // a := 1
+
 	/* End of program */
-	else if (token_tail->type == period) // .
+	if (token_tail->type == period)
 		return;
 
-	else
-		parse_statement(token_tail); // a := 1
 }
 
 void
@@ -103,11 +108,10 @@ parse_statement(const token_t *token)
 	}
 
 	else if (token->type == beginsym) { // begin
-		next_token();
-		parse_statement(token_tail); // a := 1
+		parse_statement(next_token()); // a := 1
 		do {
 			if (token_tail->type == semicolon) // ;
-				parse_statement(token_tail); // a := 1
+				parse_statement(next_token()); // a := 1
 		} while (next_token()->type != endsym); // end
 	}
 

commit a34062aaace6881dd86124df5da2812525cdd0b6

parser: Fix end hanlding in begin block * Return if there is a single statement in block * Add error handling of semicolon to avoid endless loop
Fixes: 265071d

Signed-off-by: Jason <jason23347@gmail.com>

parser.c
@@ -82,7 +82,7 @@ parse()
 
 		parse(); // block
 
-		assert(next_token(), semicolon); // ;
+		assert(token_tail, semicolon); // ;
 		next_token();
 	}
 
@@ -91,7 +91,6 @@ parse()
 	/* End of program */
 	if (token_tail->type == period)
 		return;
-
 }
 
 void
@@ -109,9 +108,19 @@ parse_statement(const token_t *token)
 
 	else if (token->type == beginsym) { // begin
 		parse_statement(next_token()); // a := 1
+		/* Return when got an 'end' symbol,
+			or assumed to be a semicolon with afterward other statements */
+		if (token_tail->type == endsym) { // end
+			next_token();
+			return;
+		}
+
 		do {
 			if (token_tail->type == semicolon) // ;
 				parse_statement(next_token()); // a := 1
+			else
+				invalid_token(token_tail, semicolon);
+
 		} while (next_token()->type != endsym); // end
 	}
 