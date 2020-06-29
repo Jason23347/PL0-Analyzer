Jason23347/PL0-Analyzer

commit 50a105a20d8e8c0367f2ef6fb84cbaf7caf74d7f

Changed error message output to context->message
Signed-off-by: Jason <jason23347@gmail.com>

interpreter.c
@@ -25,15 +25,16 @@
 #include "context.h"
 
 void
-ident_error(const char *fmt, ...)
+ident_error(const context_t *context, const char *fmt, ...)
 {
 	extern pos_t err;
 	va_list ap;
 
 	va_start(ap, fmt);
 
-	fprintf(stderr, "interpreter:%d:%d: ", err.row, err.col);
-	vfprintf(stderr, fmt, ap);
+	sprintf(context_top(context)->message, "interpreter:%d:%d: ", err.row,
+		err.col);
+	vsprintf(context_top(context)->message, fmt, ap);
 
 	va_end(ap);
 }
@@ -44,26 +45,23 @@ ident_add(context_t *context, const token_t *token, IDENT type)
 	ident_t *id;
 
 	if (token->type != ident) {
-		ident_error("cannot add ident of \"%s\" type\n",
+		ident_error(context, "cannot add ident of \"%s\" type\n",
 			    sym2human(token->type));
 		return NULL;
 	}
 
 	if (ident_find(context, token->value)) {
-		ident_error("cannot add ident \"%s\" duplicately\n",
+		ident_error(context, "cannot add ident \"%s\" duplicately\n",
 			    token->value);
 		return NULL;
 	}
 
-	if (context->id_num < PREALLOC_ID_NUM)
-		id = context->idents + context->id_num;
-	else {
-		id = malloc(sizeof(ident_t));
-		if (!id) {
-			fprintf(stderr, "Out of memory\n");
-			exit(1);
-		}
+	if (context->id_num > PREALLOC_ID_NUM) {
+		sprintf(context_top(context)->message, "Out of memory\n");
+		exit(1);
 	}
+
+	id = context->idents + context->id_num;
 
 	id->name = token->value;
 	id->type = type;
@@ -101,7 +99,8 @@ ident_assign(const context_t *context, ident_t *id, void *value)
 
 	if (id->value) {
 		if (id->type == constvar) {
-			ident_error("cannot assign value to const %s\n",
+			ident_error(context,
+				    "cannot assign value to const %s\n",
 				    id->name);
 			return -1;
 		}
@@ -110,8 +109,8 @@ ident_assign(const context_t *context, ident_t *id, void *value)
 	if (id->type == procvar)
 		id->value = value;
 	else {
-		id->value = malloc(sizeof(int));
-		memcpy(id->value, value, sizeof(int));
+		id->value = malloc(sizeof(size_t));
+		memcpy(id->value, value, sizeof(size_t));
 	}
 
 	return 0;
@@ -130,8 +129,8 @@ ident_dump(context_t *context)
 			if (ptr->type == procvar) {
 				printf("|%14s |%9s |\n", ptr->name, "(addr)");
 			} else {
-				printf("|%14s |%9d |\n", ptr->name,
-				       *(int *)ptr->value);
+				printf("|%14s |%9ld |\n", ptr->name,
+				       *(size_t *)ptr->value);
 			}
 
 		else
@@ -142,7 +141,7 @@ ident_dump(context_t *context)
 }
 
 int
-operation(int m, SYMBOL opt, int n)
+operation(const context_t *context, int m, SYMBOL opt, int n)
 {
 	switch (opt) {
 	case plus:
@@ -154,13 +153,14 @@ operation(int m, SYMBOL opt, int n)
 	case slash:
 		return m / n;
 	default:
-		ident_error("invalid operation: \"%s\"\n", sym2human(opt));
+		ident_error(context, "invalid operation: \"%s\"\n",
+			    sym2human(opt));
 		return 0;
 	}
 }
 
 bool
-condition(int m, SYMBOL opt, int n)
+condition(const context_t *context, int m, SYMBOL opt, int n)
 {
 	switch (opt) {
 	case eql:
@@ -176,7 +176,7 @@ condition(int m, SYMBOL opt, int n)
 	case geq:
 		return m >= n;
 	default:
-		ident_error("invalid operation token: \"%s\"\n",
+		ident_error(context, "invalid operation token: \"%s\"\n",
 			    sym2human(opt));
 		return false;
 	}

parser.c
@@ -27,9 +27,10 @@ invalid_token(const token_t *token, SYMBOL assumed)
 {
 	extern pos_t err;
 
-	fprintf(stderr,
+	sprintf(context_top(context)->message,
 		"syntax:%d:%d: syntax error, expected \"%s\" but got \"%s\".\n",
-		err.row, err.col, sym2human(assumed), sym2human(token->type));
+		err.row, err.col, sym2human(assumed),
+		sym2human(context->token_tail->type));
 	exit(1);
 }
 

symbols.c
@@ -66,8 +66,8 @@ unget_char(context_t *context, int ch)
 /* Print error info if symbol cannot be recongnized */
 #define invalid_symbol()                                                       \
 	{                                                                      \
-		fprintf(stderr, "lex:%d:%d: invalid symbol: %s\n", err.row,    \
-			err.col, id);                                          \
+		sprintf(context_top(context)->message, "lex:%d:%d: invalid symbol: %s\n",   \
+			err.row, err.col, id);                                 \
 		return nul;                                                    \
 	}
 
@@ -254,7 +254,7 @@ token_add(context_t *context, int flag)
 	} else {
 		t = malloc(sizeof(token_t));
 		if (!t) {
-			fprintf(stderr, "Out of memory\n");
+			sprintf(context_top(context)->message, "Out of memory\n");
 			exit(1);
 		}
 	}

commit 18066cdf13acf1c4cbbe927be8d496c645cfaa4b

Refactored error handling using context->message Fixes: 50a105a

interpreter.c
@@ -47,13 +47,13 @@ ident_add(context_t *context, const token_t *token, IDENT type)
 	ident_t *id;
 
 	if (token->type != ident) {
-		ident_error(context, "cannot add ident of \"%s\" type\n",
+		ident_error(context, "cannot add ident of \"%s\" type",
 			    sym2human(token->type));
 		return NULL;
 	}
 
 	if (ident_find(context, token->value)) {
-		ident_error(context, "cannot add ident \"%s\" duplicately\n",
+		ident_error(context, "cannot add ident \"%s\" duplicately",
 			    token->value);
 		return NULL;
 	}
@@ -103,7 +103,7 @@ ident_assign(const context_t *context, ident_t *id, void *value)
 	if (id->value) {
 		if (id->type == constvar) {
 			ident_error(context,
-				    "cannot assign value to const %s\n",
+				    "cannot assign value to const %s",
 				    id->name);
 			return -1;
 		}
@@ -154,7 +154,7 @@ operation(const context_t *context, int m, SYMBOL opt, int n)
 	case slash:
 		return m / n;
 	default:
-		ident_error(context, "invalid operation: \"%s\"\n",
+		ident_error(context, "invalid operation: \"%s\"",
 			    sym2human(opt));
 		return 0;
 	}
@@ -177,7 +177,7 @@ condition(const context_t *context, int m, SYMBOL opt, int n)
 	case geq:
 		return m >= n;
 	default:
-		ident_error(context, "invalid operation token: \"%s\"\n",
+		ident_error(context, "invalid operation token: \"%s\"",
 			    sym2human(opt));
 		return false;
 	}

parser.c
@@ -244,7 +244,7 @@ parse_factor(context_t *context)
 
 	else {
 		extern pos_t err;
-		fprintf(stderr, "syntax:%d:%d: invalid factor\n", err.col,
+		sprintf(context->message, "syntax:%d:%d: invalid factor", err.col,
 			err.row);
 	}
 

symbols.c
@@ -260,7 +260,7 @@ token_add(context_t *context, int flag)
 	int len = strlen(id);
 	t->value = malloc(len + 1);
 	if (!t->value) {
-		fprintf(stderr, "Out of mempry\n");
+		sprintf(context->message, "Out of mempry");
 		exit(1);
 	}
 	t->type = flag;